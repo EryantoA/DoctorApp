6b471ed97803c7be328c1df745185fe1e2f90303
Eryanto Agusriadi <eryantoagusriadi793@gmail.com>
2022-09-28 22:54:56 +0700

    Home Page doctor category

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {
   DoctorCategory,
+  Gap,
   HomeProfile,
   NewsItem,
   RatedDoctor,
@@ -11,26 +12,55 @@ import {colors, fonts} from '../../utils';
 export default function Doctor() {
   return (
     <View style={styles.page}>
-      <HomeProfile />
-      <Text style={styles.welcome}>Mau konsultasi dengan siapa hari ini?</Text>
-      <DoctorCategory />
-      <DoctorCategory />
-      <DoctorCategory />
-      <DoctorCategory />
-      <Text>Top Rated Doctors</Text>
-      <RatedDoctor />
-      <RatedDoctor />
-      <RatedDoctor />
-      <Text>Good News</Text>
-      <NewsItem />
-      <NewsItem />
-      <NewsItem />
+      <View style={styles.content}>
+        <HomeProfile />
+        <Text style={styles.welcome}>
+          Mau konsultasi dengan siapa hari ini?
+        </Text>
+        <View style={styles.wrapperScroll}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <View style={styles.category}>
+              <DoctorCategory />
+              <DoctorCategory />
+              <DoctorCategory />
+              <DoctorCategory />
+            </View>
+          </ScrollView>
+        </View>
+        <View style={styles.category}>
+          <Gap width={16} />
+          <DoctorCategory />
+          <DoctorCategory />
+          <DoctorCategory />
+          <DoctorCategory />
+          <Gap width={6} />
+        </View>
+        <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
+        <RatedDoctor />
+        <RatedDoctor />
+        <RatedDoctor />
+        <Text style={styles.sectionLabel}>Good News</Text>
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  page: {paddingVertical: 30, paddingHorizontal: 16},
+  page: {
+    backgroundColor: colors.secondary,
+    flex: 1,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    paddingVertical: 30,
+    paddingHorizontal: 16,
+  },
   welcome: {
     fontSize: 20,
     fontFamily: fonts.primary[600],
@@ -38,5 +68,14 @@ const styles = StyleSheet.create({
     marginTop: 30,
     marginBottom: 16,
     maxWidth: 209,
+  },
+  category: {flexDirection: 'row'},
+  wrapperScroll: {marginHorizontal: -16},
+  sectionLabel: {
+    fontSize: 16,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 30,
+    marginBottom: 16,
   },
 });
