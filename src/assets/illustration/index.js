390ed6e612cf4ddf959db6e962be626a20682aad
Eryanto Agusriadi <eryantoagusriadi793@gmail.com>
2022-09-28 22:23:30 +0700

    Home Page Profile User

--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -1,5 +1,15 @@
-import ILLogo from './logo.svg';
+import ILCatObat from './cat-dok-obat.svg';
+import ILCatPsikiater from './cat-dok-psikiater.svg';
+import ILCatUmum from './cat-dok-umum.svg';
 import ILGetStarted from './get-started.png';
+import ILLogo from './logo.svg';
 import ILNullPhoto from './null-photo.png';
 
-export {ILLogo, ILGetStarted, ILNullPhoto};
+export {
+  ILLogo,
+  ILGetStarted,
+  ILNullPhoto,
+  ILCatObat,
+  ILCatPsikiater,
+  ILCatUmum,
+};
---
+++ src/components/molecules/DoctorCategory/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function DoctorCategory() {
+  return (
+    <View>
+      <Text>DoctorCategory component</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
