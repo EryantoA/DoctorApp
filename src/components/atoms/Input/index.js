f37b7237eb7284d045439b8126a856b2da080afa
Eryanto Agusriadi <eryantoagusriadi793@gmail.com>
2022-10-04 23:56:39 +0700

    onFocus onBlur

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, TextInput, View} from 'react-native';
 import {colors, fonts} from '../../../utils';
 
 export default function Input({label}) {
+  const [border, setBorder] = useState(colors.border);
+  const onFocusForm = () => {
+    setBorder(colors.tertiary);
+  };
+  const onFocusBlur = () => {
+    setBorder(colors.border);
+  };
   return (
     <View>
       <Text style={styles.label}>{label}</Text>
-      <TextInput style={styles.input} />
+      <TextInput
+        onFocus={onFocusForm}
+        onBlur={onFocusBlur}
+        style={styles.input(border)}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  input: {
+  input: border => ({
     borderWidth: 1,
     borderRadius: 10,
     borderColor: colors.border,
     padding: 12,
-  },
+  }),
   label: {
     fontSize: 16,
     color: colors.text.secondary,
