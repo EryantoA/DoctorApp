b5a911cae016a1f401e616e4faa9b8490ea2ba28
Eryanto Agusriadi <eryantoagusriadi793@gmail.com>
2022-10-05 23:46:29 +0700

    firebase setup

---
+++ src/config/Firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBpPEn27rmT0eChdHgEt0Hptztbgczs4A0',
+  authDomain: 'my-doctor-01-9ea38.firebaseapp.com',
+  projectId: 'my-doctor-01-9ea38',
+  storageBucket: 'my-doctor-01-9ea38.appspot.com',
+  messagingSenderId: '130252798212',
+  appId: '1:130252798212:web:97eb1a589a114149283851',
+  measurementId: 'G-6SEGXLQC0L',
+});
+
+const Firebase = firebase;
+
+export default Firebase;
