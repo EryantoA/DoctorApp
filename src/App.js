2d9b31ac0df813492a3e6503de0310b6ab4cb09a
Eryanto Agusriadi <eryantoagusriadi793@gmail.com>
2023-01-25 00:00:00 +0700

    redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useState} from 'react';
 import FlashMessage from 'react-native-flash-message';
+import {Provider} from 'react-redux';
+import store from '../src/redux/store';
 import Router from './router';
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
   return (
-    <>
+    <Provider store={store}>
       <NavigationContainer>
         <Router />
       </NavigationContainer>
       <FlashMessage position="top" />
-    </>
+      {loading && <loading />}
+    </Provider>
   );
 }
